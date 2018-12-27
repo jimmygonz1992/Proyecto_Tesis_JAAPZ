9f7946d138aa9d4067764e8941799c1bae881875
jimmygonz1992 <jimmygonzalez19920@gmail.com>
2018-12-27 12:52:06 -0500

    Mant Medidor

---
+++ src/ec/com/jaapz/controlador/BodegaMantMedidorC.java
@@ -0,0 +1,28 @@
+package ec.com.jaapz.controlador;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class BodegaMantMedidorC {
+	@FXML private TextField txtCodigo;
+	@FXML private TextField txtMarca;
+	@FXML private TextField txtModelo;
+	@FXML private TextField txtPrecio;
+	@FXML private TextField txtIdMedidor;
+	@FXML private Button btnGrabar;
+	@FXML private Button btnNuevo;
+
+	
+	public void initialize() {
+		
+	}
+	
+	public void grabar() {
+		
+	}
+	
+	public void nuevo() {
+		
+	}
+}
