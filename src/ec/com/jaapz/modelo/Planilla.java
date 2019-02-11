05b2dec603c88b620795339aca707760746e8177
ByronAlex20 <alexisramrodri20@gmail.com>
2019-02-11 09:37:01 +0100

    claseDAO

--- src/ec/com/jaapz/modelo/Planilla.java
+++ src/ec/com/jaapz/modelo/Planilla.java
@@ -30,22 +30,13 @@ import java.util.List;
 			+ "where p.cuentaCliente.idCuenta = :idCuenta and p.estado = 'A' and p.aperturaLectura.estadoApertura = 'EN PROCESO' "
 			+ "order by p.idPlanilla desc"),
 	@NamedQuery(name="Planilla.buscarNoPlanillado", query="SELECT p FROM Planilla p "
-<<<<<<< HEAD
-			+ "where p.cuentaCliente.idCuenta = :idCuenta and p.estado = 'A' and p.identificadorProceso = 'SIN PLANILLAR' "
-			+ "order by p.idPlanilla desc"),	
-=======
 			+ "where p.cuentaCliente.idCuenta = :idCuenta and p.estado = 'A' and p.identificadorProceso = 'SIN PLANILLAR' "),	
->>>>>>> 19b1e9c9bcf6a32e8deb2f32312c97da3a22552f
 	@NamedQuery(name="Planilla.ListaPlanillaPendPago", query="SELECT p FROM Planilla p "
 			+ "where (lower(p.cuentaCliente.cliente.apellido) like :patron or lower(p.cuentaCliente.cliente.nombre) like :patron or lower(p.cuentaCliente.cliente.cedula) like :patron) and p.cancelado = 'PENDIENTE' and p.estado = 'A' "
 			+ "order by p.idPlanilla desc"),
 	@NamedQuery(name="Planilla.ListaPlanillaDeudaGeneral", query="SELECT p FROM Planilla p "
 			+ "WHERE p.fecha between :fechaInicio and :fechaFin and p.cancelado = 'PENDIENTE' and p.estado = 'A' "
 			+ "order by p.idPlanilla desc"),
-<<<<<<< HEAD
-	
-=======
->>>>>>> 19b1e9c9bcf6a32e8deb2f32312c97da3a22552f
 	@NamedQuery(name="Planilla.buscarPlanillaApertura", query="SELECT p FROM Planilla p "
 			+ "where p.estado = 'A' and p.aperturaLectura.idApertura = :idApertura order by p.idPlanilla"),
 	@NamedQuery(name="Planilla.ListaPlanillaPendPago", query="SELECT p FROM Planilla p "
