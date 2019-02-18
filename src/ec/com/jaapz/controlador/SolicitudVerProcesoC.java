0e552d487242305d88207e8f2fb37ed16c05d5e9
jimmygonz1992 <jimmygonzalez19920@gmail.com>
2019-02-18 11:56:40 -0500

    18_02

---
+++ src/ec/com/jaapz/controlador/SolicitudVerProcesoC.java
@@ -0,0 +1,16 @@
+package ec.com.jaapz.controlador;
+
+import java.text.SimpleDateFormat;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class SolicitudVerProcesoC {
+	@FXML TextField txtBuscar;
+	//@FXML TableView<SolInspeccionRep> tvDatos;
+	SimpleDateFormat formateador = new SimpleDateFormat("dd/MM/yyyy");
+	
+	public void initialize() {
+		
+	}
+}
