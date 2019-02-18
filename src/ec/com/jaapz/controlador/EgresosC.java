e24afbd917c32e6063554a270143049d6c58eede
jimmygonz1992 <jimmygonzalez19920@gmail.com>
2019-02-18 13:22:02 -0500

    egresos pantalla

---
+++ src/ec/com/jaapz/controlador/EgresosC.java
@@ -0,0 +1,30 @@
+package ec.com.jaapz.controlador;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class EgresosC {
+	@FXML private DatePicker dtpFecha;
+	@FXML private TextArea txtDescripcion;
+	@FXML private TextField txtValorMonto;
+	@FXML private TextArea txtObservaciones;
+	@FXML private TextField txtUsuario;
+	
+	@FXML private Button btnGrabar;
+	@FXML private Button btnNuevo;
+	
+	public void initialize(){
+		
+	}
+	
+	public void grabar() {
+		
+	}
+	
+	public void nuevo() {
+		
+	}
+}
